71d0e82b6a35067c0ce00f01984674ef12731ef9
araga <araga@example.com>
2026-04-18 15:44:05 +0200

    chore: add Google Sheet URL to config

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const weddingConfig = {
     partner2: "John",
     initial1: "K",
     initial2: "J",
-    
+
     // Event Date & Time (ISO format: YYYY-MM-DDTHH:mm:ss)
     eventDateTime: "2026-06-02T17:00:00",
     rsvpDeadline: "August 1st, 2026",
-    
+
     // Location Details
     venueName: "Nile-Ritz Carlton",
     venueAddress: "Cairo",
     mapLink: "https://maps.app.goo.gl/HSw5QR8ynjA4DxEEA",
 
     // RSVP Google Sheet Integration (leave empty if not ready)
-    googleSheetScriptUrl: ""
+    googleSheetScriptUrl: "https://docs.google.com/spreadsheets/d/1K60wE9-pC8CXGy_zVqLJ21h01tgC4TgADgMijGTl_74/edit?usp=sharing"
 };
