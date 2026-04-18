0bddce1bd6ad91c7dcb80085daf833a2ee5e9e5a
BlushMoments <felecielahaut@gmail.com>
2026-04-18 15:24:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const weddingConfig = {
     // Names and Initials
-    partner1: "Laila",
-    partner2: "Kareem",
-    initial1: "L",
-    initial2: "K",
+    partner1: "Katherine",
+    partner2: "John",
+    initial1: "K",
+    initial2: "J",
     
     // Event Date & Time (ISO format: YYYY-MM-DDTHH:mm:ss)
-    eventDateTime: "2026-05-02T18:00:00",
-    rsvpDeadline: "April 1st, 2026",
+    eventDateTime: "2026-06-02T17:00:00",
+    rsvpDeadline: "August 1st, 2026",
     
     // Location Details
-    venueName: "Armed Forces Hotel",
-    venueAddress: "Al Arish, North Sinai",
-    mapLink: "https://maps.app.goo.gl/rpKD4vcebtujBz3L6"
+    venueName: "Nile-Ritz Carlton",
+    venueAddress: "Cairo",
+    mapLink: "https://maps.app.goo.gl/HSw5QR8ynjA4DxEEA"
 };
